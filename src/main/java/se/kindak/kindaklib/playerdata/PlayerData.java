6fe3aab061a61a5951eac03b1e52cfc36de895eb
Kindak1 <hazem_bizzybone@hotmail.com>
2018-03-31 00:17:14 +0200

    - KindakLib class - Party Class

--- src/main/java/se/kindak/kindaklib/playerdata/PlayerData.java
+++ src/main/java/se/kindak/kindaklib/playerdata/PlayerData.java
@@ -5,17 +5,14 @@ import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import se.kindak.kindaklib.language.MessageComponent;
-import se.kindak.kindaklib.playerdata.util.Party;
 
 import java.util.UUID;
 
 public class PlayerData {
     private UUID playerUUID;
-    private Party party;
 
     public PlayerData(UUID playerUUID) {
         this.playerUUID = playerUUID;
-        this.party = new Party(this);
     }
 
     public void msg(String message) {
@@ -42,19 +39,11 @@ public class PlayerData {
         return Bukkit.getPlayer(playerUUID);
     }
 
-    public void setPlayerUUID(UUID playerUUID) {
-        this.playerUUID = playerUUID;
-    }
-
     public UUID getPlayerUUID() {
         return playerUUID;
     }
 
-    public Party getParty() {
-        return party;
-    }
-
-    public void setParty(Party party) {
-        this.party = party;
+    public void setPlayerUUID(UUID playerUUID) {
+        this.playerUUID = playerUUID;
     }
 }
