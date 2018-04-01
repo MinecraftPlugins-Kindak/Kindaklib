511b2c2a545f7cf95f3cd1068f1a667f5e7cc0e5
Kindak1 <hazem_bizzybone@hotmail.com>
2018-04-01 14:25:41 +0200

    + Fixed radius collector

--- src/main/java/se/kindak/kindaklib/location/Distance.java
+++ src/main/java/se/kindak/kindaklib/location/Distance.java
@@ -12,15 +12,15 @@ public class Distance {
         return location1.distance(location2) < radius;
     }
 
-    public static List<Block> getNearbyBlocks(Location location, int radius) {
+    public static List<Block> getBlocksInRadius(Location location, int radius) {
         List<Block> blocks = new ArrayList<Block>();
-        for(int x = location.getBlockX() - radius; x <= location.getBlockX() + radius; x++) {
-            for(int y = location.getBlockY() - radius; y <= location.getBlockY() + radius; y++) {
-                for(int z = location.getBlockZ() - radius; z <= location.getBlockZ() + radius; z++) {
-                    blocks.add(location.getWorld().getBlockAt(x, y, z));
+            for(int x = location.getBlockX() - radius; x <= location.getBlockX() + radius; x++) {
+                for(int y = location.getBlockY() - radius; y <= location.getBlockY() + radius; y++) {
+                    for(int z = location.getBlockZ() - radius; z <= location.getBlockZ() + radius; z++) {
+                        blocks.add(location.getWorld().getBlockAt(x, y, z));
+                    }
                 }
             }
-        }
         return blocks;
     }
 }
