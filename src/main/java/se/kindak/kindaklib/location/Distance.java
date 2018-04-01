db9d5ac6f972ff7c3249d12664b969bda4f6ab1c
Kindak1 <hazem_bizzybone@hotmail.com>
2018-04-01 13:58:48 +0200

    + Added Distance class check distances between location, gets blocks in radius.

--- src/main/java/se/kindak/kindaklib/location/Distance.java
+++ src/main/java/se/kindak/kindaklib/location/Distance.java
@@ -14,23 +14,31 @@ public class Distance {
 
     public static List<Block> getBlocksInRadius(Location location, int radius) {
         List<Block> blocks = new ArrayList<>();
+        //positivs
         for (int x = 0; x < radius; x++) {
             blocks.add(location.add(x, 0, 0).getBlock());
+            location.subtract(x, 0, 0);
         }
         for (int y = 0; y < radius; y++) {
             blocks.add(location.add(0, y, 0).getBlock());
+            location.subtract(0, y, 0);
         }
         for (int z = 0; z < radius; z++) {
             blocks.add(location.add(0, 0, z).getBlock());
+            location.subtract(0, 0, z);
         }
+        // negatives
         for (int x = 0; x < radius; x++) {
             blocks.add(location.subtract(x, 0, 0).getBlock());
+            location.add(x, 0, 0);
         }
         for (int y = 0; y < radius; y++) {
             blocks.add(location.subtract(0, y, 0).getBlock());
+            location.add(0, y, 0);
         }
         for (int z = 0; z < radius; z++) {
             blocks.add(location.subtract(0, 0, z).getBlock());
+            location.add(0, 0, z);
         }
         return blocks;
     }
