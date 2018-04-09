ee6fe183a94d498bde1a75e187cadc4adee90f46
Kindak1 <hazem_bizzybone@hotmail.com>
2018-04-09 17:59:21 +0200

    + Null check for locations

--- src/main/java/se/kindak/kindaklib/location/LocationFormater.java
+++ src/main/java/se/kindak/kindaklib/location/LocationFormater.java
@@ -6,7 +6,7 @@ import org.bukkit.World;
 
 public class LocationFormater {
 
-    public static Location format(String formatedLocation) throws Exception {
+    public static Location format(String formatedLocation) {
         String[] parts = formatedLocation.split(",");
         World world = Bukkit.getWorld(parts[0]);
         Integer x = Integer.parseInt(parts[1]);
@@ -14,13 +14,13 @@ public class LocationFormater {
         Integer z = Integer.parseInt(parts[3]);
 
         if (x == null || y == null || z == null || world == null)
-            throw new Exception("KindakLib: " + world.getName() + "," + x + "," + y + "," + z + " is not a valid Location!\nKindakLib: Requested by the location: " + formatedLocation);
+            return null;
 
         return new Location(world, x, y, z);
 
     }
 
-    public static String format(Location location) throws Exception {
+    public static String format(Location location) {
         return location.getWorld().getName() + "," + location.getBlockX() + "," + location.getBlockY() + "," + location.getBlockZ();
     }
 }
